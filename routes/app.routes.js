a57f0a8053701f4f76de2e7d686a3034e299dc78
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-06-04 10:46:44 +0100

    user (app/locals) vs user (DB). #if ../pokemon

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -145,40 +145,22 @@ router.get("/pokemon-search", async (req, res, next) => {
   }
 });
 
-//CATCH POKEMON
-
-router.get("/catch-pokemon/:id", isLoggedIn, async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const pokemonInArray = await Pokemon.find({ id: id });
-    await User.findByIdAndUpdate(req.session.user._id, {
-      $addToSet: { pokemon: pokemonInArray[0]._id },
-    });
-    if (req.session.user.team.length < 6){
-      await User.findByIdAndUpdate(req.session.user._id, {
-        $addToSet: { team: pokemonInArray[0]._id  },
-      });
-    }
-    res.redirect("/app/own-pokemon-team");
-  } catch (err) {
-    next(err);
-  }
-});
-
 //OWN PROFILE
 
 router.get("/own-profile", isLoggedIn, async (req, res, next) => {
   try {
-    const teamLength = req.session.user.pokemon.length;
-    res.render("app/own-profile", { teamLength });
+    const userId = req.session.user._id;
+    const user = await User.findById(userId);
+    res.render("app/own-profile", { user });
   } catch (err) {
     next(err);
   }
 });
-
 router.get("/own-profile-edit", isLoggedIn, async (req, res, next) => {
   try {
-    res.render("app/own-profile-edit");
+    const userId = req.session.user._id;
+    const user = await User.findById(userId);
+    res.render("app/own-profile-edit", { user });
   } catch (err) {
     next(err);
   }
@@ -186,14 +168,36 @@ router.get("/own-profile-edit", isLoggedIn, async (req, res, next) => {
 
 router.post("/own-profile-edit", async (req, res, next) => {
   try {
-    const userObjId = req.session.user._id;
+    const userId = req.session.user._id;
     const { name, username, image, description, password } = req.body;
     await User.findByIdAndUpdate(
-      userObjId,
+      userId,
       { name, username, image, description },
       { new: true }
     );
     res.redirect("own-profile");
+  } catch (err) {
+    next(err);
+  }
+});
+
+//CATCH POKEMON
+
+router.get("/catch-pokemon/:id", isLoggedIn, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const pokemonInArray = await Pokemon.find({ id: id });
+    const userId = req.session.user._id;
+    const user = await User.findById(userId);
+    await User.findByIdAndUpdate(user._id, {
+      $addToSet: { pokemon: pokemonInArray[0]._id },
+    });
+    if (user.team.length < 6){
+      await User.findByIdAndUpdate(user._id, {
+        $addToSet: { team: pokemonInArray[0]._id  },
+      });
+    }
+    res.redirect("/app/own-pokemon-team");
   } catch (err) {
     next(err);
   }
@@ -228,7 +232,7 @@ router.get("/own-pokemon-team-edit/", isLoggedIn, async (req, res, next) => {
       } else {
         if (user.pokemon.length <= 6){
           const pokemonInTeam = user.team;
-          res.render("app/own-pokemon-team-edit", { pokemonInTeam});
+          res.render("app/own-pokemon-team-edit", { pokemonInTeam });
         } else {
           //const arrayOfI= [];
           for (let i=0; i < user.pokemon.length; i++){
