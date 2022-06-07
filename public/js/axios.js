c430a4ce91351ddc624cf7863776abc0c1d45ceb
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-06-08 00:11:23 +0100

    axios not working. ?reason

--- public/js/axios.js
+++ public/js/axios.js
@@ -1,31 +1,104 @@
 window.onload = async () => {
-    const addPokemonsToTeamBtn = document.querySelectorAll('.add-pokemon-to-team-btn');
-    addPokemonsToTeamBtn.forEach(button => {
-        button.addEventListener('click', async (event) => {
-            console.log(event.currentTarget.children[0].innerHTML)
-            const id = event.currentTarget.children[0].innerHTML
-            console.log(id)
-            console.log(id)
-            await axios.post(`http://localhost:3000/app/own-pokemon-team-edit-remove/${id}`)
-            const response = await axios.get('http://localhost:3000/app/own-pokemon-team-edit/json-list')
-            response.data.forEach((pokemon) => {
 
-            })
-        })
-    })
+  const generatePokemons = async () => {
 
-    const removePokemonsFromTeamBtn = document.querySelectorAll('.remove-pokemon-from-team-btn');
-    removePokemonsFromTeamBtn.forEach(button => {
-        button.addEventListener('click', async (event) => {
-            console.log(event.currentTarget.children[0].innerHTML)
-            const id = event.currentTarget.children[0].innerHTML
-            console.log(id)
-            await axios.post(`http://localhost:3000/app/own-pokemon-team-edit-remove/${id}`)
-            const response = await axios.get('http://localhost:3000/app/own-pokemon-team-edit/json-list')
-            response.data.forEach((pokemon) => {
+    const teamDiv = document.getElementById("edit-team-team-div");
+    const pokemonDiv = document.getElementById("edit-team-pokemon-div");
+    const addPokemonsToTeamBtn = document.querySelectorAll(
+      ".add-pokemon-to-team-btn"
+    );
+    const removePokemonsFromTeamBtn = document.querySelectorAll(
+      ".remove-pokemon-from-team-btn"
+    );
 
-            })
-        })
-    })
-
-}
+    addPokemonsToTeamBtn.forEach((button) => {
+      button.addEventListener("click", async (event) => {
+        const id = event.currentTarget.children[0].innerHTML;
+        await axios.post(`http://localhost:3000/app/own-pokemon-team-edit-add/${id}`);
+        const response = await axios.get(
+          "http://localhost:3000/app/own-pokemon-team-edit/json-list"
+        );
+        teamDiv.innerHTML = "";
+        pokemonDiv.innerHTMl = "";
+        response.data[0].forEach((pokemon) => {
+          teamDiv.innerHTML += 
+          `
+          <div class="row edit-team-body">
+            <div class="col">
+                <img src="${pokemon.sprites.front_default}" alt="https://res.cloudinary.com/dvru7nv6q/image/upload/v1654107302/pokemon-tawny-port/pokeball-png-45332_rarrzq.png">
+            </div>
+            <div class="col">
+                <a>${pokemon.name} #${pokemon.id} CREATED WITH DOM</a>
+            </div>
+            <div class="col">
+              <button type="submit "class="remove-pokemon-from-team-btn"><span class="hidden">${pokemon.id}</span></button>
+            </div>
+          </div>
+          `;
+        });
+        response.data[1].forEach((pokemon) => {
+          pokemonDiv.innerHTML += 
+          `
+          <div class="row edit-team-body">
+            <div class="col">
+                <img src="${pokemon.sprites.front_default}" alt="https://res.cloudinary.com/dvru7nv6q/image/upload/v1654107302/pokemon-tawny-port/pokeball-png-45332_rarrzq.png">
+            </div>
+            <div class="col">
+                <a>${pokemon.name} #${pokemon.id} CREATED WITH DOM</a>
+            </div>
+            <div class="col">
+                <button type="submit "class="add-pokemon-to-team-btn"><span class="hidden">${pokemon.id}</span></button>
+            </div>
+          </div>
+          `;
+        });
+      });
+    });
+    removePokemonsFromTeamBtn.forEach((button) => {
+      button.addEventListener("click", async (event) => {
+        console.log(event.currentTarget.children[0].innerHTML);
+        const id = event.currentTarget.children[0].innerHTML;
+        await axios.post(`http://localhost:3000/app/own-pokemon-team-edit-remove/${id}`)
+        const response = await axios.get(
+          "http://localhost:3000/app/own-pokemon-team-edit/json-list"
+        );
+        teamDiv.innerHTML = "";
+        pokemonDiv.innerHTMl = "";
+        response.data[0].forEach((pokemon) => {
+          teamDiv.innerHTML += 
+          `
+          <div class="row edit-team-body">
+            <div class="col">
+                <img src="${pokemon.sprites.front_default}" alt="https://res.cloudinary.com/dvru7nv6q/image/upload/v1654107302/pokemon-tawny-port/pokeball-png-45332_rarrzq.png">
+            </div>
+            <div class="col">
+                <a>${pokemon.name} #${pokemon.id} CREATED WITH DOM</a>
+            </div>
+            <div class="col">
+              <button type="submit "class="remove-pokemon-from-team-btn"><span class="hidden">${pokemon.id}</span></button>
+            </div>
+          </div>
+          `;
+        });
+        response.data[1].forEach((pokemon) => {
+          pokemonDiv.innerHTML += 
+          `
+          <div class="row edit-team-body">
+            <div class="col">
+                <img src="${pokemon.sprites.front_default}" alt="https://res.cloudinary.com/dvru7nv6q/image/upload/v1654107302/pokemon-tawny-port/pokeball-png-45332_rarrzq.png">
+            </div>
+            <div class="col">
+                <a>${pokemon.name} #${pokemon.id}  CREATED WITH DOM</a>
+            </div>
+            <div class="col">
+                <button type="submit "class="add-pokemon-to-team-btn"><span class="hidden">${pokemon.id}</span></button>
+            </div>
+          </div>
+          `;
+        });
+        generatePokemons();
+      });
+    });
+  };
+  generatePokemons();
+};
